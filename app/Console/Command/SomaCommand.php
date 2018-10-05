673b12536d15f00763569458a760e153b46b0d5a
lucas.moreaux <lucasrabelomoreaux@gmail.com>
2018-10-05 18:45:54 -0300

    json e command

---
+++ app/Console/Command/SomaCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+
+class SomaCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'furia:soma{numeros}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'command para somar';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $numeros = $this->argument('numeros');
+        if(empty($numeros)){
+            $numeros = rand(0,100) .' + '. rand(0,100). ' + ' . rand(0,100);
+        }
+        $numeros_separados = explode('+', $numeros);
+        $soma = 0;
+        foreach($numeros_separados as $number){
+            $soma += $number;
+        }
+        dd($soma);
+
+    }
+}
---
+++ app/Http/Controllers/ArrayController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 05/10/2018
+ * Time: 17:45
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArrayController extends Controller
+{
+    public function returnArray(){
+
+        $dados = [
+            'status' => true,
+            'message' => 'Dados enviados não é possivel converter para json'
+        ];
+        $payload = [
+            ['id' => 1, 'name' => 'João'],
+            ['id' => 2, 'name' => 'Pedro'],
+            ['id' => 3, 'name' => 'Pedro'],
+        ];
+        $json = array();
+        foreach ($payload as $item){
+            $json[] = $item;
+        }
+
+        if(is_array($payload)){
+            return json_encode($json);
+        } else {
+            return json_encode($dados);
+        }
+    }
+
+    public function postSoma(Request $request){
+
+        $numeros = $request->numeros;
+        $numeros_separados = explode(',', $numeros);
+        //dd($numeros_separados);
+        $soma = 0;
+        foreach($numeros_separados as $number){
+            $soma += $number;
+        }
+        return $soma;
+    }
+}
